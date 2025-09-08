38b1e9c55a192c21ad97d422385e14e31baf9001
meakaliaG <mlg2225@g.rit.edu>
2025-09-08 11:28:34 -0400

    danmemes!

---
+++ src/imageResponses.js
@@ -0,0 +1,11 @@
+const fs = require('fs'); // pull in file system module
+
+const sponge = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getSponge = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(sponge);
+  response.end();
+};
+
+module.exports.getSponge = getSponge;
